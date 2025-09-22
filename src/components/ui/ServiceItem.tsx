6a39d74b1b3da0e6c5d2db41ee0d67440ac2d3e5
Rohan <shingaderohan96@gmail.com>
2025-09-22 21:39:00 +0530

    expandable section updated

--- src/components/ui/ServiceItem.tsx
+++ src/components/ui/ServiceItem.tsx
@@ -23,18 +23,31 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
 }) => {
   return (
     <div
-      className="px-4 py-4 border border-gray-600 rounded-4xl cursor-pointer transition-all duration-300"
+      className="p-4 border border-gray-600 rounded-xl md:rounded-4xl cursor-pointer transition-all duration-500"
     >
-      <div className="flex justify-between items-center" onClick={onToggle}>
+      <button
+        type="button"
+        className="flex justify-between items-center w-full bg-transparent cursor-pointer focus:outline-none"
+        aria-expanded={isExpanded}
+        tabIndex={0}
+        onClick={onToggle}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === " ") {
+            e.preventDefault();
+            onToggle();
+          }
+        }}
+      >
         <h3 className="text-lg lg:text-xl ">{service.name}</h3>
-        <button
-          className={`h-8 w-8 bg-[#cbfb45] text-black rounded-full flex items-center justify-center transition-transform duration-300 transform ${
-            isExpanded ? "rotate-45" : ""} ${darkMode ? "border border-gray-600" : ""}`}
-          aria-expanded={isExpanded}
-        >
-          <LuPlus size={20} />
-        </button>
-      </div>
+        <span>
+          <span
+            className={`h-8 w-8 p-1 bg-[#cbfb45] text-black rounded-full flex items-center justify-center transition-transform duration-300 transform ${
+              isExpanded ? "rotate-45" : ""} ${darkMode ? "border border-gray-600" : ""}`}
+          >
+            <LuPlus size={20} />
+          </span>
+        </span>
+      </button>
 
       {isExpanded && (
         <div className="pt-6">
@@ -51,7 +64,7 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
             <div className="w-full lg:w-1/2">
               <h4 className="text-3xl mb-4">{service.name}</h4>
               <p className="text-base lg:text-lg mb-6">{service.description}</p>
-              <button className="bg-[#171717] text-white px-6 py-3 rounded-full text-sm font-medium hover:bg-gray-800 transition-colors">
+              <button className="bg-[#171717] text-white px-6 py-3 rounded-full text-sm font-medium hover:text-[#cbfb45] transition-colors duration-400">
                 Learn more
               </button>
             </div>
