263d2f6f45bcb00e670bc95059c8ccc151a99e62
Rohan <shingaderohan96@gmail.com>
2025-09-25 22:44:53 +0530

    temp links added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 import Logo from './common/logo/Logo'
 import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
+import Link from 'next/link'
 
 const Footer = () => {
 
-  // Added border back to match the "boxed" mobile design
   const mobileBoxClasses = 'p-4 rounded-lg w-full';
 
   return (
@@ -21,8 +21,6 @@ const Footer = () => {
 
           <div className='w-full lg:w-[550px] flex flex-col gap-4 bg-[#cbfb45] lg:mt-14 p-4 rounded-[20px] mt-8'>
             <span className='text-gray-600 text-sm'> Join our newsletter to stay up to date. </span>
-
-
             <div className='flex flex-col sm:flex-row gap-4'>
               <input
                 type="text"
@@ -35,20 +33,18 @@ const Footer = () => {
           </div>
         </div>
 
-
         <div className='grid grid-cols-2 lg:flex lg:flex-row lg:gap-20 mt-10 lg:mt-0 gap-5'>
-
 
           <div className={`${mobileBoxClasses} lg:p-0 lg:border-none lg:rounded-none lg:min-h-0`}>
             <h4 className='font-semibold mb-6'>Quick Links</h4>
             <ul className='space-y-4 text-sm lg:text-[12px] text-gray-500'>
-              <li><a href="/" className='hover:underline'>About us</a></li>
-              <li><a href="/" className='hover:underline'>Services</a></li>
-              <li><a href="/" className='hover:underline'>Case studies</a></li>
-              <li><a href="/" className='hover:underline'>Case studies CMS</a></li>
-              <li><a href="/" className='hover:underline'>Blog</a></li>
-              <li><a href="/" className='hover:underline'>Blog post CMS</a></li>
-              <li><a href="/" className='hover:underline'>Contact</a></li>
+              <li> <Link href="/" className='hover:underline'>About us</Link> </li>
+              <li> <Link href="/" className='hover:underline'>Services</Link> </li>
+              <li> <Link href="/" className='hover:underline'>Case studies</Link> </li>
+              <li> <Link href="/" className='hover:underline'>Case studies CMS</Link> </li>
+              <li> <Link href="/" className='hover:underline'>Blog</Link> </li>
+              <li> <Link href="/" className='hover:underline'>Blog post CMS</Link> </li>
+              <li> <Link href="/" className='hover:underline'>Contact</Link> </li>
             </ul>
           </div>
 
@@ -56,60 +52,57 @@ const Footer = () => {
             <h4 className='font-semibold mb-6'>Contact</h4>
             <ul className='space-y-4 text-sm lg:text-[12px] text-gray-500'>
               <li>012 34 567 890</li>
-              <li>info@nova.agency</li>
-              <li className='max-w-[200px]'>Nova Office Complex 13 Imaginary Street Manchester</li>
+              <li>info@vision.agency</li>
+              <li className='max-w-[200px]'>VisionCode Office Complex 13 Imaginary Street Manchester</li>
             </ul>
           </div>
 
           <div className={`${mobileBoxClasses} lg:p-0 lg:border-none lg:rounded-none lg:min-h-0 col-span-2 lg:col-span-1`}>
             <h4 className='font-semibold mb-6'>Follow us</h4>
             <ul className='space-y-4 text-sm lg:text-[12px] text-gray-500'>
-              
-              <li className='flex items-center gap-3'>
+              <Link href="www.facebook.com" target='_blank' className='flex items-center gap-3'>
                 <span className='bg-[#171717] p-1.5 rounded-full'>
                   <FaFacebook className='text-lg text-[#cbfb45]' />
                 </span>
-                <a href="/" className='hover:underline'>Facebook</a>
-              </li>
-              <li className='flex items-center gap-3'>
+                <span className='hover:underline'>Facebook</span>
+              </Link>
+
+              <Link href="www.instagram.com" target='_blank' className='flex items-center gap-3'>
                 <span className='bg-[#171717] p-1.5 rounded-full'>
                   <FaInstagram className='text-lg text-[#cbfb45]' />
                 </span>
-                <a href="/" className='hover:underline'>Instagram</a>
-              </li>
-              <li className='flex items-center gap-3'>
+                <span className='hover:underline'>Instagram</span>
+              </Link>
+
+              <Link href="www.x.com" target='_blank' className='flex items-center gap-3'>
                 <span className='bg-[#171717] p-1.5 rounded-full'>
-                  <FaXTwitter className='text-lg text-[#cbfb45]' />
+                  <FaXTwitter className='text-lg text-[#cbfb45]'/>
                 </span>
-                <a href="/" className='hover:underline'>X</a>
-              </li>
-              <li className='flex items-center gap-3'>
+                <span className='hover:underline'>X</span>
+              </Link>
+
+              <Link href="www.linkedin.com" target='_blank' className='flex items-center gap-3'>
                 <span className='bg-[#171717] p-1.5 rounded-full'>
                   <FaLinkedin className='text-lg text-[#cbfb45]' />
                 </span>
-                <a href="/" className='hover:underline'>LinkedIn</a>
-              </li>
+                <span className='hover:underline'>LinkedIn</span>
+              </Link>
             </ul>
           </div>
 
         </div>
       </div>
 
-      {/* Separator Line */}
-      <div className=' border-t border-gray-700'/>
+      <div className='border-t border-gray-700'/>
 
-      {/* Copyright Section - Stacked vertically on mobile, row on tablet/desktop */}
       <div className='flex flex-col sm:flex-row justify-between items-center py-6 text-[12px] text-gray-500 gap-2 sm:gap-0'>
         <span>
           &copy; 2025 - <span className='underline'> VisionCode </span>
         </span>
-
         <span>
-          Developed by Rohan with <span className='text-red-500'>❤</span> | All Rights Reserved
+          Developed by Rohan with <span className='text-red-500'> ❤ </span> | All Rights Reserved
         </span>
       </div>
-
-
     </div>
   )
 }
